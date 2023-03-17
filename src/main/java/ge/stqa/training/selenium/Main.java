786f737aef3c8c60bb653eff5c352be1aaafee69
artem-kharchenko <artem.kharchenkoo@gmail.com>
2023-03-17 15:03:05 +0200

    Initial commit

---
+++ src/main/java/ge/stqa/training/selenium/Main.java
@@ -0,0 +1,8 @@
+package ge.stqa.training.selenium;
+
+public class Main {
+    public static void main (String [] args) {
+        System.out.println("Verification and Validation");
+
+    }
+}
---
+++ src/test/java/ge/stqa/training/selenium/ExampleTest.java
@@ -0,0 +1,36 @@
+package ge.stqa.training.selenium;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class ExampleTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.navigate().to("http://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver"));
+    }
+
+    @AfterEach
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
